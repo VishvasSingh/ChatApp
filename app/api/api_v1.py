85924c578b228f953e56dc10ce155a38bf48a1d0
vishvassingh <vishvassingh.10816@gmail.com>
2025-03-14 22:52:01 +0530

    Implemented login endpoint, fetching current user details (JWT encode/decode)

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from app.controllers import smoke
 from app.controllers import chat
+from app.controllers import login
 
 api_v1 = APIRouter()
 
 api_v1.include_router(router=smoke.router, prefix="/smoke", tags=["Smoke"])
 api_v1.include_router(router=chat.router, prefix="/chat")
+api_v1.include_router(router=login.router, prefix="/login")
