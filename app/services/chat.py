4576ccd777a2fa6a815321fc6b9cf5550679ee25
vishvassingh <vishvassingh.10816@gmail.com>
2025-03-14 19:22:48 +0530

    Implemented Singleton web sockets connection, to broadcast message to all active users

--- app/services/chat.py
+++ app/services/chat.py
@@ -10,22 +10,28 @@ app = FastAPI()
 
 
 class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
+    _instance = None
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._active_connections = list()
+
+        return cls._instance
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self._active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        self._active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: dict):
         message = json.dumps(message)
-        for connection in self.active_connections:
+        for connection in self._active_connections:
             await connection.send_text(message)
         self.save_message(message)
 
